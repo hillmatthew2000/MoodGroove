32d957a84d348bfaa0d38e4b5d0917d2bd92e4a2
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-08-20 08:18:28 -0500

    update

--- MGBlazorApp/obj/Debug/net9.0/MGBlazorApp.AssemblyInfo.cs
+++ MGBlazorApp/obj/Debug/net9.0/MGBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MGBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b91204b711a41707ad633a49bbff12d2ac52c09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8458ba3c9d108599a6866dcd2140d65acef5a07f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MGBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MGBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
